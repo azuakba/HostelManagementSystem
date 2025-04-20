495842ddc91768325efc5c9b6a85a36c3e35d2d6
azuakba <merenozzie@gmail.com>
2025-04-20 22:26:16 +0530

    re-edit admin dash

--- app/src/main/java/com/swn/hostelmanagementsystem/ui/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/swn/hostelmanagementsystem/ui/admin/AdminDashboardActivity.kt
@@ -10,41 +10,15 @@ import com.swn.hostelmanagementsystem.R
 import com.swn.hostelmanagementsystem.ui.student.Student
 
 class AdminDashboardActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var studentAdapter: StudentAdapter
-    private val studentList = mutableListOf<Student>()
-    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_dashboard)
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        studentAdapter = StudentAdapter(studentList)
-        recyclerView.adapter = studentAdapter
-
-        fetchStudents()
     }
 
-    private fun fetchStudents() {
-        val db = FirebaseFirestore.getInstance()
-        db.collection("users")
-            .whereEqualTo("role", "student")  // Fetch only students
-            .get()
-            .addOnSuccessListener { documents ->
-                studentList.clear()
-                for (document in documents) {
-                    val student = document.toObject(Student::class.java).copy(id = document.id)
-                    studentList.add(student)
-                }
-                studentAdapter.notifyDataSetChanged()
-            }
-            .addOnFailureListener { e ->
-                Log.e("Firestore", "Error fetching students", e)
-            }
-    }
+
 
 
 }
